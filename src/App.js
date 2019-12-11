275b8037980c3b48de7f01abcd2f595501fb1125
Ivan Rozinko <ivanrozya@gmail.com>
2019-12-11 23:54:00 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Jumbotron, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [artist, setArtist] = useState('');
+
+  const handleClick = () => {
+    console.log('click');
+    fetchData();
+  };
+
+  const fetchData = () => {
+    const prefix = 'https://cors-anywhere.herokuapp.com/';
+    const query = encodeURI(artist);
+    console.log(query);
+    const deezerUrl = `${prefix}https://api.deezer.com/search?q=${query}}`;
+    const itunesUrl = `${prefix}https://itunes.apple.com/search?term=${query}}`;
+
+    const promiseDeezer = fetch(deezerUrl);
+    const promiseItunes = fetch(itunesUrl);
+
+    const resDeezer = promiseDeezer.then( res => res.json());
+    const resItunes = promiseItunes.then( res => res.json());
+
+    let list = [];
+    resDeezer.then(res => {
+        res.data.map( artist => {
+          const {
+            artist: { name },
+            album: { title, cover_medium }
+          } = artist;
+
+          list.push(
+            {
+              name,
+              album: title,
+              image: cover_medium,
+
+            })
+        });
+        console.log(list);
+    }
+
+    );   // save next link to fetch more
+    resItunes.then(data => console.log(data.results));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+     <Jumbotron className="mt-5">
+     <Row className="justify-content-center">
+       <h1>Searcher</h1>
+     </Row>
+     <Row className="justify-content-center">
+       <Form>
+         <Form.Group>
+           <Col className="d-flex">
+             <Form.Control
+               type="text"
+               placeholder="Red hot chili peppers"
+               value={artist}
+               onChange={(event) => setArtist(event.target.value)}
+             />
+             <Button
+               variant="warning"
+               onClick={handleClick}
+             >
+               Go!
+             </Button>
+           </Col>
+         </Form.Group>
+       </Form>
+     </Row>
+     </Jumbotron>
+   </Container>
   );
 }
 
